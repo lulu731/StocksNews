df7f8f082d3d03c48256d9954f93f9f8aa383f03
lulu731 <lulu731lulu@hotmail.fr>
2018-03-07 22:41:19 +0100

    https_client class inspired by alexandruc

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # StocksNews
+
+https_client class inspired by alexandruc (https://github.com/alexandruc/SimpleHttpsClient).
---
+++ Tests/https_client.hpp
@@ -0,0 +1,32 @@
+#ifndef HTTPS_CLIENT_H
+#define HTTPS_CLIENT_H
+
+#include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
+
+using boost::asio::ip::tcp;
+
+class client
+{
+public:
+    client(boost::asio::io_service& io_service,
+        boost::asio::ssl::context& context,
+        const std::string& server,
+        const std::string& path);
+
+private:
+    void handle_resolve(const boost::system::error_code& err, tcp::resolver::iterator endpoint_iterator);
+    bool verify_certificate(bool preverified, boost::asio::ssl::verify_context& ctx);
+    void handle_connect(const boost::system::error_code& err);
+    void handle_handshake(const boost::system::error_code& error);
+    void handle_write_request(const boost::system::error_code& err);
+    void handle_read_status_line(const boost::system::error_code& err);
+    void handle_read_headers(const boost::system::error_code& err);
+    void handle_read_content(const boost::system::error_code& err);
+    tcp::resolver resolver_;
+    boost::asio::ssl::stream<boost::asio::ip::tcp::socket> socket_;
+    boost::asio::streambuf request_;
+    boost::asio::streambuf response_;
+};
+
+#endif
