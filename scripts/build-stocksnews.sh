42210a34f5c25fa7b7e713e78e949c3f9167f7b8
lulu731 <lulu731lulu@hotmail.fr>
2018-05-19 12:05:48 +0200

    Updated build.

--- scripts/build-stocksnews.sh
+++ scripts/build-stocksnews.sh
@@ -4,8 +4,9 @@ lulu731=/home/travis/build/lulu731
 (
   cd /home/travis/build/lulu731/StocksNews || exit
   mkdir build
-  cd build
+  cd build || exit
   cmake -G "Unix Makefiles" ..
   make
+  cp -r ../stocks/ .
   cd ..
 )
--- scripts/test-units.sh
+++ scripts/test-units.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 stocksnews=/home/travis/build/lulu731/StocksNews
-cd $stocksnews/tests
-mkdir build
-cd build
-cmake -G "Unix Makefiles" ..
-make
-./tests
-cd $stocksnews 
-
-
+(
+ cd $stocksnews/build/tests || exit
+ ./tests
+ cd $stocksnews 
+)
