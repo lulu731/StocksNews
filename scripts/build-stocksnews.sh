4482fb86ca39bccb0d9ffb25cb2042e7da8d0969
lulu731 <lulu731lulu@hotmail.fr>
2018-05-21 21:18:02 +0200

    Updated scripts.

--- scripts/build-stocksnews.sh
+++ scripts/build-stocksnews.sh
@@ -4,10 +4,12 @@ lulu731=/home/travis/build/lulu731
 (
   cd /home/travis/build/lulu731/StocksNews || exit
   mkdir build
-  cd build || exit
+  cd build
   cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ..
   make
+  exit_val = $?
   cp -r ../stocks/ .
   cd ..
-  exit $?
 )
+exit $exit_val 
+
